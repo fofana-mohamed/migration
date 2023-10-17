6083b331baf69290782ab7c84c04d529196f2693
fofana-mohamed <mohamed.fofana63@yahoo.com>
2023-10-17 18:56:55 -0400

    initial commit

---
+++ src/test/java/com/kanfory/ecole/EcoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kanfory.ecole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
